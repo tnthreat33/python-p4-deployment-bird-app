95d462b5bdc61f662dcb1946f92d7fec78051956
Rachel Hamby <tnthreat33@gmail.com>
2023-12-10 15:42:01 -0500

    issues with render connection

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify, request, make_response, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Bird
+from models import db, Bird 
 
 app = Flask(
     __name__,
